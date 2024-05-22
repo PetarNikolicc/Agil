e1f6371e7415bd2ab483b801655b678b505cef88
Kvisslan <lobstercak3@gmail.com>
2024-05-22 13:57:39 +0200

    ändrar lite i koden för att det ska fungera på pythonanywhere.

--- app.py
+++ app.py
@@ -1,43 +1,43 @@
 from flask import Flask, render_template, render_template, request, redirect, url_for, jsonify
 import sqlite3
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Text
-import os
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import Column, Integer, String, Text
+# import os
 import pandas as pd
 
 app = Flask(__name__)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Database.db')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-class Recipes(db.Model):
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    minutes = Column(String)
-    n_steps	= Column(String)
-    steps = Column(String)
-    ingredients	= Column(String)
-    n_ingredients = Column(String)
+# class Recipes(db.Model):
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Text)
+#     minutes = Column(String)
+#     n_steps	= Column(String)
+#     steps = Column(String)
+#     ingredients	= Column(String)
+#     n_ingredients = Column(String)
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    file = os.path.join(basedir, "data2.csv")
-    df = pd.read_csv(file)
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
+#     file = os.path.join(basedir, "data2.csv")
+#     df = pd.read_csv(file)
 
-    for index, row in df.iterrows():
-        recipe = Recipes(
-            name = row["name"],
-            minutes = row["minutes"],
-            n_steps = row["n_steps"],
-            steps = row["steps"],
-            ingredients = row["ingredients"],
-            n_ingredients = row["n_ingredients"]
-        )
-        db.session.add(recipe)
-    db.session.commit()
+#     for index, row in df.iterrows():
+#         recipe = Recipes(
+#             name = row["name"],
+#             minutes = row["minutes"],
+#             n_steps = row["n_steps"],
+#             steps = row["steps"],
+#             ingredients = row["ingredients"],
+#             n_ingredients = row["n_ingredients"]
+#         )
+#         db.session.add(recipe)
+#     db.session.commit()
 
 def get_recipes_by_time(max_time):
     conn = sqlite3.connect('recipes.db')
@@ -47,13 +47,10 @@ def get_recipes_by_time(max_time):
     conn.close()
     return recipes
 
-
-
 @app.route("/")
 def homepage():
-    recipes = Recipes.query.all()
-    return render_template("index.html", recipes=recipes)
-
+    # recipes = Recipes.query.all()
+    return render_template("index.html") #, recipes=recipes
 
 @app.route('/filter', methods=['GET'])
 def filter_recipes():
