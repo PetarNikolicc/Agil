bc1604d77e6092ca50aa25343f66547b980cd907
Jamuna <jamuna1493@gmail.com>
2024-05-30 12:17:20 +0200

    added hero section andmodified little in app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
-def get_recipes_by_time(max_time):
+def get_recipes_by_time(exact_time):
     db_path = os.path.join(os.path.dirname(__file__), 'recipes.db')
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM recipes WHERE cooking_time <= ?", (max_time,))
+    cursor.execute("SELECT * FROM recipes WHERE cooking_time = ?", (exact_time,))
     recipes = cursor.fetchall()
     conn.close()
     return recipes
@@ -18,10 +18,16 @@ def get_recipes_by_time(max_time):
 def homepage():
     time = request.args.get('time')
     if time:
-        recipes = get_recipes_by_time(time)
+        try:
+            time = int(time)
+            recipes = get_recipes_by_time(time)
+        except ValueError:
+            recipes = []
     else:
-        recipes = get_recipes_by_time(60)
+        recipes = []
     return render_template('index.html', recipes=recipes)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
