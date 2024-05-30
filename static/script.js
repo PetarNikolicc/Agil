fbcfb766842c3cb827edea91792733c980304817
PriscilaDiaz79 <prisciladiaz@hotmail.com>
2024-05-30 15:32:58 +0200

    Lägg till ny fil script.js

---
+++ static/script.js
@@ -0,0 +1,15 @@
+function openPopup(title, image, ingredients, instructions) {
+    document.getElementById('popupTitle').innerText = title;
+    document.getElementById('popupImage').src = image;
+
+    let ingredientsList = document.getElementById('popupIngredients');
+    ingredientsList.innerHTML = '';
+    ingredients.split('\\n').forEach(ingredient => {
+        let li = document.createElement('li');
+        li.innerText = ingredient.trim(); // trim för att ta bort extra mellanslag
+        ingredientsList.appendChild(li);
+    });
+
+    document.getElementById('popupInstructions').innerText = instructions.trim().replace(/\\n/g, '\n'); // trim för att ta bort extra mellanslag
+    document.getElementById('recipePopup').style.display = 'block';
+}
