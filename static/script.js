8d8e24c0404289c7c848ed71375614cb93e31753
PriscilaDiaz79 <prisciladiaz@hotmail.com>
2024-05-30 15:07:10 +0200

    Spara nuvarande ändringar

--- static/script.js
+++ static/script.js
@@ -1,9 +1,15 @@
-function openPopup(title, text) {
+function openPopup(title, image, ingredients, instructions) {
     document.getElementById('popupTitle').innerText = title;
-    document.getElementById('popupText').innerText = text;
-    document.getElementById('recipePopup').style.display = 'flex';
-}
+    document.getElementById('popupImage').src = image;
 
-function closePopup() {
-    document.getElementById('recipePopup').style.display = 'none';
+    let ingredientsList = document.getElementById('popupIngredients');
+    ingredientsList.innerHTML = '';
+    ingredients.split('\\n').forEach(ingredient => {
+        let li = document.createElement('li');
+        li.innerText = ingredient.trim(); // trim för att ta bort extra mellanslag
+        ingredientsList.appendChild(li);
+    });
+
+    document.getElementById('popupInstructions').innerText = instructions.trim().replace(/\\n/g, '\n'); // trim för att ta bort extra mellanslag
+    document.getElementById('recipePopup').style.display = 'block';
 }
