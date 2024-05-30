2c606f1a8e576cf5eb148f2f6dc85fdf1a418f01
PriscilaDiaz79 <prisciladiaz@hotmail.com>
2024-05-31 00:18:44 +0200

    ändrat tillbaka app.py och i script.js så att det gick att stänga popupen

--- app.py
+++ app.py
@@ -4,26 +4,7 @@ import os
 
 app = Flask(__name__)
 
-def get_recipes_by_time(exact_time):
-    db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './recipes.db'))
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM recipes WHERE cooking_time = ?", (exact_time,))
-
-    results = cursor.fetchall()
-    conn.close()
-    return results
-
-DATABASE = './recipes.db'
-
-def update_recipe_ingredients(recipe_updates):
-    db_path = os.path.join(os.path.dirname(__file__), 'recipes.db')
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    for name, ingredients in recipe_updates.items():
-        cursor.execute("UPDATE recipes SET ingredients = ? WHERE name = ?", (ingredients, name))
-    conn.commit()
-    conn.close()
+DATABASE = 'recipes.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -31,14 +12,6 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-def add_ingredients_column():
-    db_path = os.path.join(os.path.dirname(__file__), 'recipes.db')
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("ALTER TABLE recipes ADD COLUMN ingredients TEXT;")
-    conn.commit()
-    conn.close()
-    
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -48,26 +21,12 @@ def close_connection(exception):
 def get_recipes_by_time(time):
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT name, cooking_time, image_path, instructions, FROM recipes WHERE cooking_time = ?", (time,))
-
+    cursor.execute("SELECT name, cooking_time, image_path, ingredients, instructions FROM recipes WHERE cooking_time = ?", (time,))
     recipes = cursor.fetchall()
     return recipes
 
-"""@app.route('/')
+@app.route('/')
 def homepage():
-    time = request.args.get('time')
-    if time:
-        try:
-            time = int(time)
-            recipes = get_recipes_by_time(time)
-        except ValueError:
-            recipes = []
-    else:
-        recipes = []
-    return render_template('index.html', recipes=recipes)"""
-
-@app.route('/', methods=['GET'])
-def index():
     return render_template('index.html')
 
 @app.route('/time', methods=['GET'])
